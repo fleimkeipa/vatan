7e244468cecca2442cf33edee4acb21e99c36463
fleimkeipa <adem_sahin12@hotmail.com>
2021-10-28 17:10:07 +0300

    preload func added

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -34,6 +34,7 @@ func main() {
 	//feature=2&&category=2 it returns products which feature id equals 2 and category id equals 2
 	//selled=2&&category=2 it return products which selled id equals 2 and category id equals 2
 	//deleted=2&&feature=2 it return products which deleted id equals 2 and feature id equals 2
+	//all parameters = selled=productId && deleted=productId && feature=featureId && category=categoryId
 	r.GET("/stocks/filter", product.Filter)
 	r.POST("/stock/insert", product.Insert)
 	r.PUT("/stock/:id/update", product.Update)
